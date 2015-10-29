27c57cc56e580765b0094a1741eea5562b19a1ab
Zak Estrada <zestrad2@illinois.edu>
2015-10-29 12:43:40 -0500

    Initial add of objdump to control flow graph

---
+++ objdump_cfg/objdump_to_cfg.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+#
+#Analyze a kernel using objdump, listing which functions call which instructions
+#instructions of interest are given as a file of newline-delimited regexes
+#(text version, at&t syntax).
+#
+#We take an unzipped kernel source and a System.map to bound functions.
+#If you don't have the unzipped version, you can use scripts/extract-vmlinux
+#from the kernel.org tree to get it.  Likewise, you can use scripts/mksysmap to
+#generate that one.
+#
+
+import re
+import sys
+import subprocess
+import string
+
+#Build a CFG for a function based on objdump -d
+#usage: objdump_to_cfg.py "objdump -d output" function_name
+
+#jumps from
+#http://blog.fairchild.dk/2013/06/automagical-asm-control-flow-arrow-annotation/
+jumps = { #define jumps, synomyms on same line
+'ja':'if above', 'jnbe':'if not below or equal',
+'jae':'if above or equal','jnb':'if not below','jnc':'if not carry',
+'jb':'if below', 'jnae':'if not above or equal', 'jc':'if carry',
+'jbe':'if below or equal', 'jna':'if not above',
+'jcxz':'if cx register is 0', 'jecxz':'if cx register is 0',
+'je':'if equal', 'jz':'if zero',
+'jg':'if greater', 'jnle':'if not less or equal',
+'jge':'if greater or equal',
+'jl':'if less', 'jnge':'if not greater or equal',
+'jle':'if less or equal', 'jnl':'if not less',
+'jmp':'unconditional',
+'jne':'if not equal', 'jnz':'if not zero',
+'jng':'if not greater',
+'jno':'if not overflow',
+'jnp':'if not parity', 'jpo':'if parity odd',
+'jns':'if not sign',
+'jo':'if overflow',
+'jp':'if parity', 'jpe':'if parity even',
+'js':'if sign'}
+
+calls = {'call': 'call', 'callq':'call qword'} 
+
+dump_file = sys.argv[1]
+root_name = sys.argv[2] #The function that will be the root of our CFG
+
+#below regexes based on those from Lok
+symbol_re = re.compile("^([a-fA-F0-9]+) <([\.\w]+)>:\s*$") 
+symbol_plt_re = re.compile("^([a-fA-F0-9]+) <([@\w]+)>:\s*$")
+
+#TODO: handle conditionals
+print "#address;[target1, target2, ...]" #our output format
+
+#CFG: key is source address, values are all targets
+#     note that source=0 means root and source=-1 means we came from an indirect
+CFG={}
+
+root=0 #The root of our CFG
+
+#Store the objdump in memory, list of tuples (address, asm)
+#Went with this over a dict since we should be iterating more than searching
+#so having it sorted by address just makes sense
+objdump=[]
+indirect_count=0
+
+def main():
+  with open(dump_file) as f:
+    for line in f:
+      #First find out function of interest
+      m=symbol_re.match(line)
+      if(m):
+        if(m.group(2) == root_name):
+          print(m.group(1), m.group(2))
+          root=int(m.group(1),16)
+
+      #Example tab-delimited output:
+      #ADDRESS    INSTRUCTION           ASCII
+      #c1000000:  8b 0d 80 16 5d 01     mov    0x15d1680,%ecx
+      fields = string.split(line.rstrip(),'\t')
+      
+      #Symbols have 2 fields, but their actual first instruction will have 3
+      if(len(fields)<3):
+        continue
+      objdump.append((int(fields[0].replace(":",""),16), fields[2]))
+
+  if(root==0):
+    print "Could not find root function for CFG: %s" % root_name
+
+  #Recursively process BBs until we get a return to 0
+  iterate_bb(0, root, [0])
+  print_CFG()
+  print("Indirect calls/jumps: %d"%indirect_count)
+
+####END MAIN BEGIN FUNCTION DEFS#####
+def print_CFG():
+  for(key, value) in CFG.iteritems():
+    print("0x%x: %s"%(key, string.join('0x%x' % t for t in value)))
+
+def get_objdump_index(address):
+  first = 0
+  last = len(objdump)-1
+
+  while first<=last:
+    mid = (first+last)/2
+    item = objdump[mid][0]
+    if(item==address):
+      return mid
+    else:
+      if(address < item):
+        last = mid-1
+      else:
+        first = mid+1
+  print("couldn't find address: %s"%address) 
+  raise ValueError
+
+def add_cfg_edge(source,dest):
+  if(source not in CFG):
+    CFG[source]=[]
+  CFG[source].append(dest)
+
+def print_instr(instr):
+    print("0x%x: %s"%(objdump[instr][0],objdump[instr][1]))
+
+def iterate_bb(source, blockaddr, callstack):
+  global indirect_count
+  #Stop condition
+  if(blockaddr==0):
+    return
+  add_cfg_edge(source, blockaddr)
+  #Now, step through until we hit a jump, call, or ret
+  for i in range(get_objdump_index(blockaddr),len(objdump)):
+    #instr, target = (0,0)
+    try:
+      (instr, target) = string.split(objdump[i][1])
+    except ValueError:
+      #no target means no branch, walk right on by
+      continue
+
+    #Look for jumps
+    if(instr in jumps):
+      if '*' in target:
+        #Count it, abandon all hope
+        indirect_count+=1
+        return
+      if(instr == "jmp"):
+        #unconditional
+        return iterate_bb(target, callstack[:])
+      else:
+        #Jump taken
+        iterate_bb(blockaddr, target, callstack[:])
+        #Jump not taken
+        iterate_bb(blockaddr, objdump[i+1][0], callstack[:])
+        return    
+
+    #Look for calls 
+    if(instr in calls):
+      #Indirect, just skip over it as if we returned
+      if '*' in target:
+        indirect_count+=1
+        return iterate_bb(-1, objdump[i+1][0], callstack[:])
+      else: #for readability
+        #Tried appending within a slice, but it was unhappy
+        newstack=callstack[:]
+        newstack.append(objdump[i+1][0])
+        return iterate_bb(blockaddr, target, newstack)
+    if("ret" in instr):
+      iterate_bb(blockaddr, callstack[-1], callstack[:-1])
+
+main()
